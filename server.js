aa3e42c49d7382448f9df5308bd4d1abeda241cc
kabiraisha <kabira@gmatc.matc.edu>
2025-09-12 03:41:02 -0500

    Data Validation Added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import connectDatabase from './config/db.js'; 
+import connectDatabase from './config/db.js';
+import { check, validationResult } from 'express-validator';
 
 // Initialize express application
 const app = express();
@@ -8,11 +9,11 @@ const app = express();
 connectDatabase();
 
 // Configure Middleware
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 
 // API endpoints
 app.get('/', (req, res) =>
-res.send('http get request sent to root api endpoint')
+    res.send('http get request sent to root api endpoint')
 );
 
 // API endpoints
@@ -29,10 +30,22 @@ res.send('http get request sent to root api endpoint')
  * @route POST api/users
  * @desc Register user
  */
- app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
- });
+app.post('/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ], (req, res) => {
+        // console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            // Database to be saved here later
+            res.send(req.body);
+        }
+    }
+);
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));

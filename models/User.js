66181b10a1e5b5de19b1e954ed003aaff176e4b9
kabiraisha <kabira@gmatc.matc.edu>
2025-09-12 01:41:07 -0500

    User model created

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// Import the mongoose module
+import mongoose from 'mongoose';
+
+// Create a schema for your model named UserSchema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+});
+
+// Using your UserSchema, generate a model named User
+const User = mongoose.model('user', UserSchema);
+
+// Export the User model to make it accessible to the rest of your application
+export default User;

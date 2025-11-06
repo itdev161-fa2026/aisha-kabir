c96311cfc27b985e1f4f09771c102e7417b59b7a
kabiraisha <kabira@gmatc.matc.edu>
2025-11-06 02:18:51 -0600

    Complete Activity 8: User Authentication Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider';
 import Header from './components/Header';
 import Home from './pages/Home';
 import PostDetail from './pages/PostDetail';
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts/:id" element={<PostDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
-export default App
+export default App;
+
